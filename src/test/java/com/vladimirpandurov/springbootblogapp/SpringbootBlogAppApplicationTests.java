33af75330d67fb8fd23561eff2271c704b3719f2
panvla <pandvlado@gmail.com>
2022-02-23 22:08:42 +0100

    adding springboot project

---
+++ src/test/java/com/vladimirpandurov/springbootblogapp/SpringbootBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.springbootblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
